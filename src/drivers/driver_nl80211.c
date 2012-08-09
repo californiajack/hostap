752a638568988a645f2feb0cabe2cbbc711216f2
Dmitry Shmidt <dimitrysh@google.com>
2012-08-09 18:29:46 +0300

    nl80211: Fix message processing
    This is from wpa_supplicant_8.git commit
    6e95f5f46d53a4c40386e2ef9ec87e56af9e2160. Though, the "break;" line
    itself was added by an earlier commit and this new commit was just
    fixing the code by adding the forgotten curly brackets:
    
    BUG: 6584268
    
    Change-Id: Ib840d6b678a914be4e50d4916fecc1738b2b56f9
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- src/drivers/driver_nl80211.c
+++ src/drivers/driver_nl80211.c
@@ -2337,8 +2337,12 @@ static int process_global_event(struct nl_msg *msg, void *arg)
 	dl_list_for_each_safe(drv, tmp, &global->interfaces,
 			      struct wpa_driver_nl80211_data, list) {
 		if (ifidx == -1 || ifidx == drv->ifindex ||
-		    have_ifidx(drv, ifidx))
+		    have_ifidx(drv, ifidx)) {
 			do_process_drv_event(drv, gnlh->cmd, tb);
+#ifdef ANDROID_P2P
+			break;
+#endif /* ANDROID_P2P */
+		}
 	}
 
 	return NL_SKIP;
